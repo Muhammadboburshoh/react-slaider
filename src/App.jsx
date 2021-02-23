ff3991cc5082807e091d145115722c91f6ef79dd
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-02-23 10:44:38 +0500

    Edit container style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Slider from "./Componints/Slider/Slider"
+import Search from "./Componints/Search/Search"
 
 function App() {
   return (
-    <>
+    <div className="container">
     <Slider />
-    </>
+    <br/>
+    <br/>
+    <Search />
+    </div>
   )
 }
 
